f335e11f431608ae68fb9df14da55769fd71a724
Susan Wangari <56164343+Susan-Wangari@users.noreply.github.com>
2022-02-18 13:04:38 +0300

    Info component

---
+++ Components/Info.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function About () {
+    return (
+        <div className="about">
+        <h3>About</h3>
+        <p>I am a knowledgeable front-end developer adept at creating successful websites that meet customer needs. Specializing in collaborating with customers to gather requirements, produce plans and improve plans for usability and functionality. Fully proficient in HTML5, CSS3, Bootstrap and React JS.</p>
+        </div>
+    )
+}
