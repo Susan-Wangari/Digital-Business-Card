a83d844c8a9c4f20fbe23b4ab7d88def6371d0e3
Susan Wangari <56164343+Susan-Wangari@users.noreply.github.com>
2022-02-18 13:05:17 +0300

    interests components

---
+++ Components/Interests.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Interests () {
+    return (
+        <div>
+         <p>Software development. Developer Relations. Crotcheting. Travel.</p>
+        </div>
+    )
+}
